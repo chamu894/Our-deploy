72a2e569476faa47c16fa6e51c56a3477e263ff0
chamudith bandara <chamudithbuddima@gmail.com>
2024-03-28 20:56:02 +0530

    new

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,28 +10,28 @@ import Image from "next/image";
 const Details = ({ position, name, companyLinks, time, address, work }) => {
     const ref = useRef(null);
   return (
-    <li ref={ref} className="my-8 sm:text-center first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]">
+    <li ref={ref} className="my-16 xl:my-5 lg:my-10 md:my-6 sm:text-start first:mt-0 last:mb-0 w-[60%] xl:w-[70%] xs:w-[90%] mx-auto flex flex-col  items-center justify-between md:w-[80%]">
         <LiIcon reference={ref}/>
       <motion.div
-      initial={{y:50}}
-      whileInView={{y:0}}
-      transition={{duration:0.5, type:"spring"}}
+      initial={{y:70}}
+      whileInView={{y:10}}
+      transition={{duration:1, type:"spring"}}
       >
         
-        <h3 className=" capitalize font-bold text-2xl sm:text-xl xs:text-lg ">
+        <h3 className=" capitalize font-bold ml-10 text-2xl xl:text-lg sm:text-xl xs:text-lg ">
           {position}&nbsp;<br/>
           <a
             href={companyLinks}
             target="_blank"
-            className="text-primary dark:text-primaryDark capitalize hover:underline"
+            className=" text-primary dark:text-primaryDark capitalize hover:underline"
           >
             @{name}
           </a>
         </h3>
-        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
+        <span className="ml-10 capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {time} | {address}
         </span>
-        <p className=" font-medium w-full md:text-sm">{work}</p>
+        <p className=" font-medium w-full xl:text-sm md:text-xs ml-10">{work}</p>
       </motion.div>
     </li>
   );
@@ -42,7 +42,7 @@ const Experience = () => {
     const {scrollYProgress} = useScroll(
         {
             target: ref,
-            offset:["start end","center start"]
+            offset:["start center"," end center"]
         }
     )
   return (
@@ -55,22 +55,22 @@ const Experience = () => {
 
       <motion.div
       style={{scaleY: scrollYProgress}} 
-      className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light 
-      md:w-[2px] md:left-[30px] xs:left-[20px]" />
+      className="absolute left-9  top-0 w-[4px] h-full bg-dark origin-top dark:bg-light 
+      md:w-[2px] md:left-[29px] xs:left-[20px]" />
 
-        <ul className="w-full flex flex-col items-start justify-between sm:ml-3 xs:ml-2">
+        <ul className="w-full flex flex-col items-start  justify-between sm:ml-3 xs:-ml-4">
           
-            <>
+            <div className="flex mb-10 sm:flex-col">
 
             <div
-              className="ml-[20%] col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark 
+              className="ml-[20%] sm:w-[50%] col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark 
             bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:col-span-8"
             >
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image
                 src={ChamudithBandara}
                 alt="ChamudithBandara"
-                className=" w-[250px] md:w-[175px] sm:w-[150px] h-auto rounded-2xl"
+                className=" w-[250px] md:w-[175px] sm:w-[150px] h-auto rounded-2xl "
                 priority
                 sizes="(max-width: 768px) 100vw,
                 (max-width: 1200px) 50vw,
@@ -83,19 +83,17 @@ const Experience = () => {
             companyLinks="https://www.linkedin.com/in/chamudith-bandara-930975202/"
             name="CHAMUDITH BANDARA"
             time="2022-Present"
-            address="Acres 7, Dekinda Junction, Balana, Kadugannawa."
             work="Worked on a team responsible for developing new features for Google's 
                 search engine, including improving the accuracy and relevance of search results and 
                 developing new tools for data analysis and visualization."
           />
 
+          </div>
 
-
-            
-          </>
+          <div className="flex mb-10 sm:flex-col">
 
           <div
-              className="ml-[20%] col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark 
+              className=" ml-[20%] sm:w-[50%] col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark 
             bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:col-span-8"
             >
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
@@ -115,18 +113,20 @@ const Experience = () => {
             companyLinks="https://www.linkedin.com/in/tharindu-marasinghe-16443a217/"
             name="THARINDU MARASINGHE"
             time="2022-Present"
-            address="232/1 kotabogoda, kadugannawa"
             work="Worked on a team responsible for developing new features for Google's 
                 search engine, including improving the accuracy and relevance of search results and 
                 developing new tools for data analysis and visualization."
           />
 
+
+          </div>
+
           
           
-          <>
+          <div className="flex mb-10 sm:flex-col">
 
           <div
-              className="ml-[20%] col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark 
+              className="ml-[20%] sm:w-[50%] col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark 
             bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:col-span-8"
             >
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
@@ -146,13 +146,12 @@ const Experience = () => {
             companyLinks=""
             name="LAHIRU BANDARANAYAKA"
             time="2022-Present"
-            address="E3,Aluthnuwara rd, Idampitiya, Hingula"
             work="Worked on a team responsible for developing new features for Google's 
                 search engine, including improving the accuracy and relevance of search results and 
                 developing new tools for data analysis and visualization."
           />
 
-          </>
+          </div>
 
         </ul>
       </div>
--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -8,12 +8,12 @@ const LiIcon = ({reference}) => {
     const {scrollYProgress} = useScroll(
     {
         target: reference,
-        offset:["center end","center center"]
+        offset:["start center"," end center"]
     }
 )
 
   return (
-    <figure className='absolute left-0 stroke-dark dark:stroke-light'>
+    <figure className='absolute left-0 my-24 md:my-16 sm:my-3 lg:my-20 stroke-dark dark:stroke-light'>
         <svg className='-rotate-90 md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px]' width="75" height="75" viewBox='0 0 100 100'>
             <circle cx="75" cy="50" r="20" className='stroke-primary dark:stroke-primaryDark stroke-1 fill-none'/>
             <motion.circle cx="75" cy="50" r="20" className=' stroke-[5px] fill-light dark:fill-dark' 
